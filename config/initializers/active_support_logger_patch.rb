81b58c7006620a63fb638384a7130dbca545034a
Jeremy Duncan <jpd294@cornell.edu>
2025-02-26 17:03:22 -0500

    fix: added logger

--- config/initializers/active_support_logger_patch.rb
+++
@@ -1,9 +0,0 @@
-# Fixes uninitialized constant ActiveSupport::LoggerThreadSafeLevel::Logger (NameError)
-if defined?(ActiveSupport::LoggerThreadSafeLevel)
-  require 'logger' unless defined?(Logger)
-  module ActiveSupport
-    module LoggerThreadSafeLevel
-      Logger = ::Logger unless defined?(Logger)
-    end
-  end
-end
