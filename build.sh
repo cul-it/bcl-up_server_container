641ad12b7068fe7f11605d6dc1ef9d1cd5e8d91e
Jeremy Duncan <jpd294@cornell.edu>
2025-04-14 12:44:13 -0400

    5: added comments

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 source "scripts/log_utils.sh"
 
-# ==============================================================================
-# Updates the Git-based gem `bcl_up_server` and rebuilds the Docker image
-# ------------------------------------------------------------------------------
-# This ensures that the Gemfile.lock is always using the latest commit from main
-# before Docker builds, avoiding stale references and rebuild bugs.
-# ==============================================================================
+###############################################################################
+## Updates the Git-based gem `bcl_up_server` and rebuilds the Docker image   ##
+## ------------------------------------------------------------------------- ##
+## This ensures that the Gemfile.lock is always using the latest commit from ##
+## main before Docker builds, avoiding stale references and rebuild bugs.    ##
+###############################################################################
 print_header "💻 Executing build.sh"
 
 print_msg "🛠️  Updating bcl_up_server gem from Git..."
